9a9fc45215518afc91025508d40deb1bddc34b91
2Fat <alan.lefournis@viacesi.fr>
2015-05-21 13:30:34 +0200

    Initial commit

---
+++ app/src/main/java/com/android/fishroom/bulbasaur/BootActivity.java
@@ -0,0 +1,14 @@
+package com.android.fishroom.bulbasaur;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BootActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_boot);
+	}
+}
